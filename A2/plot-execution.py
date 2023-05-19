d9004ff84537d14a58c65324ccd7ee950820d226
Lukas Hinterleitner <lukas.hinterleitner@innotechnologies.at>
2023-05-19 19:09:23 +0200

    add execution time files for ALMA cluster and a python scipt that generates a plot of the speedup

---
+++ A2/plot-execution.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+
+
+def get_arguments_from_filename(filename: str) -> str:
+    array = filename.replace(".csv", "").split("-")
+    n = array[3]
+    m = array[4]
+    max_iterations = array[5]
+    epsilon = array[6]
+
+    return f"n={n}, m={m}, max_iter={max_iterations}, \u03B5={epsilon}"
+
+
+omp_execution_time_files = glob.glob("execution-time-omp-*")
+mpi_execution_time_files = glob.glob("execution-time-mpi-*")
+
+plt.figure(figsize=(15, 8))
+
+plt.subplot(2, 1, 1)
+plt.grid()
+plt.title("omp speedup")
+plt.xlabel("number of threads")
+plt.ylabel("speedup")
+plt.xticks(np.arange(1, 33))
+plt.yticks(np.arange(1, 20))
+
+for filename in omp_execution_time_files:
+    df = pd.read_csv(filename)
+    plt.plot(df["num_thread"], df["execution-time"].iloc[0] / df["execution-time"], label=get_arguments_from_filename(filename))
+
+plt.legend()
+plt.subplot(2, 1, 2)
+for filename in mpi_execution_time_files:
+    df = pd.read_csv(filename)
+
+    plt.title("mpi speedup")
+    plt.xlabel("number of threads")
+    plt.ylabel("speedup")
+    plt.xticks()
+    plt.grid()
+
+    plt.plot(df["num_thread"], df["execution-time"].iloc[0] / df["execution-time"])
+
+plt.tight_layout()
+plt.savefig("speedup.png")
