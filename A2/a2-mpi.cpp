35836bac7a4d7066fbc387dbe66f89f5ddad4b01
Lukas Hinterleitner <lukas.hinterleitner@innotechnologies.at>
2023-06-04 11:07:19 +0200

    introduce different starting and end index for differential equation calculation for first and last process

--- A2/a2-mpi.cpp
+++ A2/a2-mpi.cpp
@@ -36,13 +36,8 @@ int main(int argc, char **argv)
         std::cout << "Configuration: m: " << M << ", n: " << N << ", max-iterations: " << max_iterations << ", epsilon: " << epsilon << std::endl;
 
     auto time_mpi_1 = MPI_Wtime();
-
-    // START: the main part of the code that needs to use MPI/OpenMP timing routines 
-    //  MPI hint: remember to initialize MPI first 
     
     int i, j;
-    double diffnorm;
-    int iteration_count = 0;
 
     // change M to local size for horizontal fragmentation
     int local_M = (M / numprocs) + 2; // +2 for padding rows
@@ -51,7 +46,7 @@ int main(int argc, char **argv)
     Mat W(local_M, N); // for MPI: use local sizes with MPI, e.g., recalculate M and N
 
     // Init & Boundary
-    for (i = 1; i < local_M - 2; ++i) {
+    for (i = 1; i < local_M - 1; ++i) {
         for (j = 0; j < N; ++j) {
             W[i][j] = U[i][j] = 0.0;
         }
@@ -60,76 +55,100 @@ int main(int argc, char **argv)
         W[i][N-1] = U[i][N-1] = 0.1; // right side
     }
 
+    // initialize top row
     if (rank == 0) {
         for (j = 0; j < N; ++j) {
             W[1][j] = U[1][j] = 0.02; // top
         }
     }
 
+    // initialize bottom row
     if (rank == (numprocs - 1)) {
         for (j = 0; j < N; ++j) {
             W[local_M - 2][j] = U[local_M - 2][j] = 0.2; // bottom
         }
     }
-
     // End init
 
-    MPI_Request request_upwards, request_downwards;
-    MPI_Status  status_upwards, status_downwards;
+    MPI_Request request_upwards[numprocs], request_downwards[numprocs];
+    MPI_Status  status_upwards[numprocs], status_downwards[numprocs];
 
-    iteration_count = 0;
+    double diffnorm;
+    double global_diffnorm;
+    int iteration_count = 0;
+
     do
     {
         iteration_count++;
         diffnorm = 0.0;
+        global_diffnorm = 0.0;
 
-        // Compute new values (but not on boundary) 
-        for (i = 1; i < local_M - 2; ++i)
-        {
-            for (j = 1; j < N - 1; ++j)
-            {
-                W[i][j] = (U[i][j + 1] + U[i][j - 1] + U[i + 1][j] + U[i - 1][j]) * 0.25;
-                diffnorm += (W[i][j] - U[i][j]) * (W[i][j] - U[i][j]);
-            }
+        // all processes except first one
+        if (rank != 0) {
+            // receive from above
+            // send upwards
+
+            // save row from above in top padding row
+            MPI_Irecv(&U[0][0], N, MPI_DOUBLE, rank - 1, DOWNWARDS_TAG, WORLD, &request_downwards[rank]);
+
+            // send first row to above process (not the padding row)
+            MPI_Send(&U[1][0], N, MPI_DOUBLE, rank - 1, UPWARDS_TAG, WORLD);
+
+            MPI_Wait(&request_downwards[rank], &status_downwards[rank]);
+
+            //MPI_Isend(U[1], N, MPI_DOUBLE, rank - 1, UPWARDS_TAG, MPI_COMM_WORLD, &request_upwards);
+            //MPI_Irecv(U[0], N, MPI_DOUBLE, rank - 1, DOWNWARDS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
-        // Only transfer the interior points
-        for (i = 1; i < local_M - 2; ++i)
-            for (j = 1; j < N - 1; ++j)
-                U[i][j] = W[i][j];
-
-        diffnorm = sqrt(diffnorm); // all processes need to know when to stop
-
-        /*
-        if (epsilon >= diffnorm) {
-            cout << "Rank: " << rank << ", Iteration: " << iteration_count << ", diffnorm: " << diffnorm << ", Epsilon: " << epsilon << endl;
-        }
-        */
-
+        // all processes except last one
         if (rank != (numprocs - 1)) {
             // receive from below
             // send downwards
 
-            MPI_Irecv(U[local_M-1], N, MPI_DOUBLE, rank + 1, UPWARDS_TAG, WORLD, &request_upwards);
-            MPI_Send(U[local_M-2], N, MPI_DOUBLE, rank + 1, DOWNWARDS_TAG, WORLD);
-            MPI_Wait(&request_upwards, &status_upwards);
+            // save row from below in bottom padding row
+            MPI_Irecv(&U[local_M-1][0], N, MPI_DOUBLE, rank + 1, UPWARDS_TAG, WORLD, &request_upwards[rank]);
+
+            // send last row to below process (not the padding row)
+            MPI_Send(&U[local_M-2][0], N, MPI_DOUBLE, rank + 1, DOWNWARDS_TAG, WORLD);
+
+            MPI_Wait(&request_upwards[rank], &status_upwards[rank]);
 
             //MPI_Isend(U[local_M-2], N, MPI_DOUBLE, rank + 1, DOWNWARDS_TAG, MPI_COMM_WORLD, &request_downwards);
             //MPI_Recv(U[local_M-1], N, MPI_DOUBLE, rank + 1, UPWARDS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
-        if (rank != 0) {
-            // receive from above
-            // send upwards
+        int start_M = 1;
+        int end_M = local_M - 1;
 
-            MPI_Irecv(U[0], N, MPI_DOUBLE, rank - 1, DOWNWARDS_TAG, WORLD, &request_downwards);
-            MPI_Send(U[1], N, MPI_DOUBLE, rank - 1, UPWARDS_TAG, WORLD);
-            MPI_Wait(&request_downwards, &status_downwards);
-
-            //MPI_Isend(U[1], N, MPI_DOUBLE, rank - 1, UPWARDS_TAG, MPI_COMM_WORLD, &request_upwards);
-            //MPI_Irecv(U[0], N, MPI_DOUBLE, rank - 1, DOWNWARDS_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        // Only compute interior points --> skip padding rows
+        if (rank == 0) {
+            start_M = 2;
+        } else if (rank == (numprocs - 1)) {
+            end_M = local_M - 2;
         }
-    } while (epsilon <= diffnorm && iteration_count < max_iterations);
+
+        // Compute new values (but not on boundary) 
+        for (i = start_M; i < end_M; ++i){
+            for (j = 1; j < N - 1; ++j)
+            {
+                W(i,j) = (U(i,j + 1) + U(i,j - 1) + U(i + 1,j) + U(i - 1,j)) * 0.25;
+                diffnorm += (W(i,j) - U(i,j))*(W(i,j) - U(i,j));
+            }
+        }
+
+        // since matrix is initialized differently on some processes, we need to sum up all diffnorms so that each process has the same stopping criterion
+        MPI_Allreduce(&diffnorm, &global_diffnorm, 1, MPI_DOUBLE, MPI_SUM, WORLD);
+
+        // Only transfer the interior points
+        for (i = start_M; i < end_M; ++i)
+            for (j = 1; j < N - 1; ++j)
+                U[i][j] = W[i][j];
+
+        global_diffnorm = sqrt(global_diffnorm); // all processes need to know when to stop
+
+        //cout << "Rank: " << rank << ", Iteration: " << iteration_count << ", diffnorm: " << diffnorm << ", Epsilon: " << epsilon << ", Global diffnorm: " << global_diffnorm << endl;
+
+    } while (epsilon <= global_diffnorm && iteration_count < max_iterations);
 
     Mat final_local_U(local_M - 2, N);
 
@@ -165,10 +184,10 @@ int main(int argc, char **argv)
         Mat U_sequential(M, N); // init another matrix for the verification
 
         int iteration_count_seq = 0;
-        heat2d_sequential(U_sequential, max_iterations, epsilon, iteration_count_seq); 
+        heat2d_sequential(U_sequential, max_iterations, epsilon, iteration_count_seq);
 
         // Here we need both results - from the sequential (U_sequential) and also from the OpenMP/MPI version, then we compare them with the compare(...) function 
-        cout << "Verification: " << ( U.compare(U_sequential) && iteration_count == iteration_count_seq ? "OK" : "NOT OK") << std::endl;
+        cout << "Verification: " << ( bigU.compare(U_sequential) && iteration_count == iteration_count_seq ? "OK" : "NOT OK") << std::endl;
     }
 
     MPI_Finalize();
